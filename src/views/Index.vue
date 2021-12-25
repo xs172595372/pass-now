7ad19738bdffa0a1e69927f0d756d8aea5f7c02a
xs172595372 <172595372@qq.com>
2021-12-25 22:59:40 +0800

    搭建首页

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,14 +1,55 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+    <el-container>
+        <el-card
+            class="exercise"
+            v-for="item in exercises"
+            :key="item.index"
+            @click.native="enter()"
+            @contextmenu.prevent.native="show()"
+        >
+            {{ item.title }}
+        </el-card>
+        <el-popover
+            v-show="visible">
+
+        </el-popover>
+    </el-container>
 </template>
 
 <script>
 
 export default {
-  name: 'Home',
-  components: {
-  }
-}
+    name: "Home",
+    components: {},
+    data() {
+        return {
+            exercises: [{
+                index: 1,
+                title: "Javascript科目二",
+            }, {
+                index: 2,
+                title: "Javascript科目三",
+            }, {
+                index: 3,
+                title: "Javascript科目四",
+            }],
+            visible: false,
+        };
+    },
+    methods: {
+        enter() {
+            this.$router.push("/detail");
+        },
+        show() {
+            this.visible = true;
+        },
+    },
+};
 </script>
+
+<style scoped>
+.exercise {
+    margin: 20px;
+    cursor: pointer;
+}
+</style>
