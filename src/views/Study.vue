8cd72a3c3866966fb4fe3d5a6d51a6a5efd1d9ca
xs172595372 <172595372@qq.com>
2021-12-26 21:58:20 +0800

    新增题库

--- src/views/Study.vue
+++ src/views/Study.vue
@@ -1,17 +1,39 @@
 <template>
     <div style="text-align: left; padding: 30px">
-
         <el-header>
             <el-button @click.native="back" style="margin-right: 20px">返回首页</el-button>
-            <span style="font-size: 20px;font-weight: bold">{{this.exercise.title}}</span>
+            <span style="font-size: 20px;font-weight: bold">{{ this.exercise.title }}</span>
         </el-header>
         <el-divider></el-divider>
         <div class="title">{{ problem.showTitle }}</div>
-
-        <div class="options" v-for="(item,index) in problem.options" :key="item.title">
-            {{ String.fromCharCode(index + 65) + "、" + item.title }}
+        <div v-if="!problem.type">
+            <el-radio-group v-model="problem.selected">
+                <el-row
+                    v-for="(item,index) in problem.options"
+                    :key="item.title"
+                >
+                    <el-checkbox class="options">
+                        {{ String.fromCharCode(index + 65) + "、" + item.title }}
+                    </el-checkbox>
+                </el-row>
+            </el-radio-group>
         </div>
+        <div v-else>
+            <el-radio-group v-model="problem.multipleSelected">
+                <el-row
+                    v-for="(item,index) in problem.options"
+                    :key="item.title"
+                >
+                    <el-radio
+                        class="options"
+                        aria-selected="false"
+                        :label="String.fromCharCode(index + 65) + '、'+ item.title"
+                    >
+                    </el-radio>
+                </el-row>
 
+            </el-radio-group>
+        </div>
         <div style="margin-top: 30px">
             <el-button type="primary" @click="submit">提交答案</el-button>
             <el-button @click="next">下一题</el-button>
@@ -29,6 +51,8 @@ export default {
             store: new Store(),
             problem: {
                 options: [],
+                selected: "",
+                multipleSelected: [],
             },
         };
     },
@@ -47,8 +71,8 @@ export default {
         this.problem.showTitle = "【" + (this.problem.type ? "单选" : "多选") + "】" + this.problem.title;
     },
     methods: {
-        submit(){
-
+        submit() {
+            console.log({selected: this.problem.selected,multipleSelected: this.problem.multipleSelected})
         },
         next() {
             this.problem = this.exercise.problems[Math.floor(Math.random() * this.exercise.problems.length)];
@@ -72,6 +96,5 @@ export default {
     font-size: 20px;
     margin-top: 30px;
     margin-left: 10px;
-    cursor: pointer;
 }
 </style>
